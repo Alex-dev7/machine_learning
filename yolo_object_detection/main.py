d0b8f9792685f418a1e1d438e25d49e3d644f36a
Alexei Rusu <rusualexei1995@gmail.com>
2024-10-29 23:01:14 -0400

    yolo object detection

---
+++ yolo_object_detection/main.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('yolo11n.pt')
+
+#load video
+video_path = './HEARTHSTONE.mp4'
+cap = cv2.VideoCapture(video_path)
+
+ret = True
+# read frame
+while ret:
+    ret, frame = cap.read()
+    # detect and track objects
+    results = model.track(frame, persist=True)
+    # plot results
+    frame_ = results[0].plot()
+    # visualize
+    cv2.imshow('frame', frame_)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+    
