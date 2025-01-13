85436559e7efcba7b3192cabfd8b28a0c3ace5cf
Alex-dev7 <rusualexei1995@gmail.com>
2025-01-12 19:54:22 -0500

    model training and plotting with matplot

--- linear_regression/readme.md
+++ linear_regression/readme.md
@@ -1,2 +1,6 @@
 #### Student grades prediction with linear regression:
+
 ![image](res.png)
+
+![image](g1.png)
+![image](f1.png)
--- linear_regression/test.py
+++ linear_regression/test.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import pickle
+from matplotlib import style
+
 
 data = pd.read_csv("student_mat.csv", sep=";")
 
@@ -15,19 +18,32 @@ predict = "G3"
 
 X = np.array(data.drop(columns=predict, axis=1))
 Y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+# ------- UNCOMMENT TO TRAIN THE MODEL
+"""best = 0
+for _ in range(60):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
-# find the best fit line
-linear.fit(x_train, y_train)
+    linear = linear_model.LinearRegression()
 
-# accuracy
-acc = linear.score(x_test, y_test)
+    # find the best fit line
+    linear.fit(x_train, y_train)
+
+    # accuracy
+    acc = linear.score(x_test, y_test)
+
+    if acc > best:
+        best = acc
+        print("Accuracy: ", acc)
+        # saving the model
+        with open("student_model.pickle", "wb") as f:
+            pickle.dump(linear, f)"""
 
 
-print("Accuracy: ", acc)
+pickle_in = open("student_model.pickle", "rb")
+linear = pickle.load(pickle_in)
+
 print("Coefficient: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
 
@@ -36,3 +52,10 @@ predictions = linear.predict(x_test)
 print("Predictions: ")
 for x in range (len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = "G1"
+style.use("ggplot")
+plt.scatter(data[p], data["G3"])
+plt.xlabel(p)
+plt.ylabel("Final Grade")
+plt.show()
